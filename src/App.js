da2e28dae560aeaa09c3eb05a5ec7cfb2cea2778
tirempbs <tirem@fastmail.com>
2019-04-19 14:43:46 -0400

    completed challenge

--- src/App.js
+++ src/App.js
@@ -1,28 +1,65 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
+const api = 'https://chroniclingamerica.loc.gov/suggest/titles/?q=brooklyn';
+
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+    state = {
+        data: null
+    }
+    
+    componentDidMount() {
+        fetch(api)
+        .then(response => response.json())
+        .then(data => this.setState({ data }))
+    }
+    
+    mapData = () => {
+        if (this.state.data) {
+            
+            let allData = []
+            const sns = this.state.data[2]
+            const cleanSns = sns.map(sn => sn.replace('sn', ''))
+            const links = this.state.data[3]
+            const titles = this.state.data[1]
+
+            cleanSns.map(sn => allData.push({ sn }))
+            links.map((link, i) => allData[i].link = link)
+            titles.map((title, i) => allData[i].title = title)
+
+            const sortedData = allData.sort(function (a, b) {
+                return a.sn - b.sn;
+            });
+
+            return sortedData.map(obj => {
+                    return (
+                        <tr key={obj.sn}>
+                            <td>{obj.sn}</td>
+                            <td><a href={obj.link}>{obj.title}</a></td>
+                        </tr>
+                    )
+                }
+            )
+
+        } //end of if check to avoid null state
+    } //end of mapData function
+    
+    render() {
+        console.log(this.state.data)
+        
+        return (
+            <table>
+                <tbody>
+                    <tr>
+                        <th>SN Number</th>
+                        <th>Link</th>
+                    </tr>
+                    {this.mapData()}
+                </tbody>
+            </table >
+        );
+    }
 }
 
 export default App;
+
